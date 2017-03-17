e2ebc849083ef4579c440092137645f832fddf46
Alex Kolchinski <y.a.kolchinski@gmail.com>
2017-03-16 22:22:11 -0700

    Implemented final neural net layer for coattention decoder

--- code/qa_sep_model.py
+++ code/qa_sep_model.py
@@ -146,8 +146,28 @@ class NaiveCoDecoder(object):
             end_probs = inner
 
 
-        #return tf.concat(2, [start_probs, end_probs])
+        with vs.variable_scope("final_start_layer"):
+            z = tf.concat(1, [start_probs, end_probs])
 
+            wfs = tf.get_variable("W_f_s", [2*c_len, 2*c_len], tf.float32, xav_init)
+            bfs = tf.get_variable("B_f_s", [2*c_len], tf.float32, tf.constant_initializer(0.0))
+            z2 = tf.nn.relu(tf.matmul(z, wfs) + bfs)
+
+            wfs2 = tf.get_variable("W_f_s2", [2*c_len, c_len], tf.float32, xav_init)
+            bfs2 = tf.get_variable("B_f_s2", [c_len], tf.float32, tf.constant_initializer(0.0))
+            start_probs = tf.nn.relu(tf.matmul(z2, wfs2) + bfs2)
+
+
+        with vs.variable_scope("final_end_layer"):
+            z = tf.concat(1, [start_probs, end_probs])
+
+            wfe = tf.get_variable("W_f_e", [2*c_len, 2*c_len], tf.float32, xav_init)
+            bfe = tf.get_variable("B_f_e", [2*c_len], tf.float32, tf.constant_initializer(0.0))
+            z2 = tf.nn.relu(tf.matmul(z, wfe) + bfe)
+
+            wfe2 = tf.get_variable("W_f_e2", [2*c_len, c_len], tf.float32, xav_init)
+            bfe2 = tf.get_variable("B_f_e2", [c_len], tf.float32, tf.constant_initializer(0.0))
+            end_probs = tf.nn.relu(tf.matmul(z2, wfe2) + bfe2)
 
 
         return start_probs, end_probs
