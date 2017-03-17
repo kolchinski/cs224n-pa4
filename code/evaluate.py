3d7c5937a11e6aa44068d543ad55de5c5f014b18
John Clow <johnwclow@hotmail.com>
2017-03-17 00:21:28 -0700

    Ugh, I really should've looked for other places where it returns....

--- code/evaluate.py
+++ code/evaluate.py
@@ -44,7 +44,7 @@ def new_f1_score(prediction, ground_truth):
     common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
     num_same = sum(common.values())
     if num_same == 0:
-        return 0
+        return 0, 0, 0
     precision = 1.0 * num_same / len(prediction_tokens)
     recall = 1.0 * num_same / len(ground_truth_tokens)
     f1 = (2 * precision * recall) / (precision + recall)
