597bcca78073f6213f29f69406ec57b6738aa3cc
Alex Kolchinski <y.a.kolchinski@gmail.com>
2017-03-16 21:04:04 -0700

    Fixed shape bug (now using single direction LSTM, results have half the shape)

--- code/qa_sep_model.py
+++ code/qa_sep_model.py
@@ -296,9 +296,9 @@ class NaiveCoDecoder(object):
                 sequence_length=lengths, dtype=tf.float32,
                 swap_memory=True)
 
-            w_s = tf.get_variable("W_s", (2*self.hidden_size, 1), tf.float32, xav_init)
+            w_s = tf.get_variable("W_s", (self.hidden_size, 1), tf.float32, xav_init)
             b_s = tf.get_variable("b_s", (1,), tf.float32, tf.constant_initializer(0.0))
-            s_h = tf.reshape(s_h, [-1, 2*self.hidden_size])
+            s_h = tf.reshape(s_h, [-1, self.hidden_size])
             inner = tf.matmul(s_h, w_s) + b_s
             inner = tf.reshape(inner, [-1, c_len])
             #start_probs = tf.nn.softmax(inner)
@@ -311,9 +311,9 @@ class NaiveCoDecoder(object):
                 sequence_length=lengths, dtype=tf.float32,
                 swap_memory=True)
 
-            w_e = tf.get_variable("W_e", (2*self.hidden_size, 1), tf.float32, xav_init)
+            w_e = tf.get_variable("W_e", (self.hidden_size, 1), tf.float32, xav_init)
             b_e = tf.get_variable("b_e", (1,), tf.float32, tf.constant_initializer(0.0))
-            e_h = tf.reshape(e_h, [-1, 2*self.hidden_size])
+            e_h = tf.reshape(e_h, [-1, self.hidden_size])
             inner = tf.matmul(e_h, w_e) + b_e
             inner = tf.reshape(inner, [-1, c_len])
             #end_probs = tf.nn.softmax(inner)
